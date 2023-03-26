792e62c06e990fb46ab1422a30a4f78073ca510b
Carlos Eduardo <cadusantos.paiva@gmail.com>
2023-03-26 07:57:00 -0300

    Removendo Arquivos

--- download.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (isset($_POST['submit'])) {
-    $url = $_POST['url'];
-    $command = "python3 youtube-dl-script.py " . $url;
-    shell_exec($command);
-    $file = getcwd() . "/downloads/" . basename($url) . ".mp3";
-    header("Location: index.php?file=$file");
-}
-?>
